cdca24a42cd8cf22e43fad6a728df3abed4327fb
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-03-31 17:26:48 -0300

    validations

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+
 //Interface
 import { TaskList } from '../../model/task-list';
 
@@ -7,14 +8,17 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> =  [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort((first, last)=> Number(first.checked) - Number(last.checked) );
   }
+
+
 
   public deleteItemTaskList(event: number){
     this.taskList.splice(event, 1);
@@ -30,6 +34,15 @@ export class TodoListComponent implements OnInit {
 
   public setEmitTaskList(event: string){
     this.taskList.push({task: event , checked: false});
+  }
+
+  public validationInput(event: string, index:  number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja DELETAR?");
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 
 }
